f80ef85aac97960bffb04b1bc6ca3d19dc4878e5
ElasticBow <79611589+ElasticBow@users.noreply.github.com>
2022-05-22 21:55:05 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+c1 = "You are appreciated"
+c2 = "Nothing is impossible"
+c3 = "Your presense is welcomed"
+c4 = "Continue on, even When it gets hard"
+c5 = "You can light up the sky, even when it's night"
+c6 = "Don't just bring out the best in others, bring out the best in yourself too"
+c7 = "Excellent job today and everyday"
+c8 = "Don't doubt your ability, your potential is endless"
+c9 = "Always trust yourself"
+c10 = "You are more special than you realize"
+c11 = "You are worthy of love"
+c12 = "Positivity over negativity"
+c13 = "Keep a growth mindset at all times"
+c14 = "Everyone deserves happiness and joy, you are no exception"
+c15 = "Determination is a powerful trait, use it to its full ability"
+
+function compliment()
+{
+    document.getElementById("cl1").innerHTML = c1;
+    document.getElementById("cl2").innerHTML = c2;
+    document.getElementById("cl3").innerHTML = c3;
+    document.getElementById("cl4").innerHTML = c4;
+    document.getElementById("cl5").innerHTML = c5;
+    document.getElementById("cl6").innerHTML = c6;
+    document.getElementById("cl7").innerHTML = c7;
+    document.getElementById("cl8").innerHTML = c8;
+    document.getElementById("cl9").innerHTML = c9;
+    document.getElementById("cl10").innerHTML = c10;
+    document.getElementById("cl11").innerHTML = c11;
+    document.getElementById("cl12").innerHTML = c12;
+    document.getElementById("cl13").innerHTML = c13;
+    document.getElementById("cl14").innerHTML = c14;
+    document.getElementById("cl15").innerHTML = c15;
+}
